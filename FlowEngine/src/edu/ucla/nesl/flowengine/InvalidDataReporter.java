bca97bca1a4aa5db1fb96556ffdc65adb3f643ab
Haksoo Choi <haksoo@cs.ucla.edu>
2014-08-21 18:12:04 -0700

    Removed FlowEngine.

--- FlowEngine/src/edu/ucla/nesl/flowengine/InvalidDataReporter.java
+++
@@ -1,9 +0,0 @@
-package edu.ucla.nesl.flowengine;
-
-public class InvalidDataReporter {
-	private static final String TAG = InvalidDataReporter.class.getSimpleName();
-	
-	public static void report(String msg) {
-		DebugHelper.log(TAG, msg);
-	}
-}
--- FlowEngine/src/edu/ucla/nesl/flowengine/aidl/DeviceAPI.aidl
+++
@@ -1,9 +0,0 @@
-package edu.ucla.nesl.flowengine.aidl;
-
-interface DeviceAPI {
-	void start();
-	void stop();
-	void kill();
-	void startSensor(int sensor);
-	void stopSensor(int sensor);
-}
